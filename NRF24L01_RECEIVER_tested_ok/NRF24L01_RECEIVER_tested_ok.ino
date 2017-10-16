6cd444de1c1d90219d8ae3f3efdea6d8e8bc7a3d
Jason Prabu. L <32505007+Jason-Prabu@users.noreply.github.com>
2017-10-17 00:06:42 +0530

    Add files via upload

---
+++ NRF24L01_RECEIVER_tested_ok/NRF24L01_RECEIVER_tested_ok.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8);
+
+const uint64_t pipes[1] = {0xF0F0F0F0E1LL};
+
+void setup()
+{
+  while (!Serial);
+  Serial.begin(9600);
+
+  radio.begin();
+  radio.openReadingPipe(0, pipes[0]);
+  radio.startListening();
+
+}
+
+void loop()
+{
+  if (radio.available())
+  {
+    unsigned long text = 0;
+    radio.read(&text, sizeof(text));
+
+    Serial.println(text);
+  }
+}
---
+++ NRF24LO1_TRANSMITTER_tested_ok/NRF24LO1_TRANSMITTER_tested_ok.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8);
+
+const uint64_t pipes[1] = {0xF0F0F0F0E1LL};
+
+
+void setup()
+{
+  radio.begin();
+  radio.setRetries(15, 15);
+  radio.openWritingPipe(pipes[0]);
+  radio.stopListening();
+}
+
+void loop()
+{
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  
+  delay(1000);
+}
